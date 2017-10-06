dd29e3bb8272448b8af8285712f40d523b3cda54
Josh Kennedy <jshknndy@gmail.com>
2017-10-06 08:26:57 -0500

    update ruby to work

--- install/gem.sh
+++ install/gem.sh
@@ -8,8 +8,8 @@ brew install gpg2
 gpg2 --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 \curl -sSL https://get.rvm.io | bash -s stable
 
-rvm install 2.4
-rvm use 2.4 --default
+rvm install 2.2.2
+rvm use 2.2.2 --default
 
 gem install lunchy
 gem install pygmentize
